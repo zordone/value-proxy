0b8a722c4568ac6dc1d855c61e7cc00eea87293d
Zord <zord.bk@gmail.com>
2017-09-03 16:20:58 +0200

    Encapsulate privates

--- src/valueProxy.js
+++ src/valueProxy.js
@@ -2,19 +2,19 @@ import _ from 'lodash';
 
 class ValueNode {
 	constructor(value, path = 'root', error) {
-    	this.$value = value;
-        this.$path = path;
-        this.$error = error;
+    	this._value = value;
+        this._path = path;
+        this._error = error;
         // lodash functions
         this.$_find = this._lodashDecorator('find', _.find, 0);
     }
     _nextValue(nextValue, nextPath) {
-        const nextError = this.$error || (
+        const nextError = this._error || (
             nextValue === undefined
             ? {
                 $path: nextPath,
-                $lastPath: this.$path,
-                $lastValue: this.$value
+                $lastPath: this._path,
+                $lastValue: this._value
             }
             : undefined
         );
@@ -24,18 +24,24 @@ class ValueNode {
         return (...params) => {
             const paramsWithValue = params
                     .slice(0, valueParamIndex)
-                    .concat([this.$value], params.slice(valueParamIndex)),
+                    .concat([this._value], params.slice(valueParamIndex)),
                 nextValue = func(...paramsWithValue),
-                nextPath = `${this.$path}.$_${funcName}(${params.map(JSON.stringify)})`;
+                nextPath = `${this._path}.$_${funcName}(${params.map(JSON.stringify)})`;
             return this._nextValue(nextValue, nextPath);
         };
     }
+    $value() {
+        return this._value;
+    }
+    $error() {
+        return this._error;
+    }
     $log() {
-    	const value = this.$value;
-    	console.group(this.$path, '=', value);
+    	const value = this._value;
+    	console.group(this._path, '=', value);
         if (value === undefined) {
-            console.log('Undefined since:',this.$error.$path);
-            console.log('Last defined value:', this.$error.$lastPath, '=', this.$error.$lastValue);
+            console.log('Undefined since:',this._error.$path);
+            console.log('Last defined value:', this._error.$lastPath, '=', this._error.$lastValue);
         }
         console.groupEnd('');
         console.log('');
@@ -46,21 +52,29 @@ class ValueNode {
 export const valueProxy = (() => {
     const handler = {
         get(obj, prop, receiver) {
-            // default behaviour for symbols and public methods
-            if (typeof prop === 'symbol' || prop.startsWith('$')) {
+            // default behaviour for symbol prop names
+            if (typeof prop === 'symbol') {
             	return Reflect.get(obj, prop);
             }
+            // auto-bind public methods to enable access to privates
+            if (prop.startsWith('$')) {
+            	let value = Reflect.get(obj, prop);
+                if (typeof value === 'function') {
+                    value = value.bind(obj);
+                }
+                return value;
+            }           
             // hide private methods
             if (prop.startsWith('_')) {
                 return;
             }
             // are we an array/object?
-            const nextValue = typeof receiver.$value === 'object'
-                    ? Reflect.get(receiver.$value, prop)
+            const nextValue = typeof obj._value === 'object'
+                    ? Reflect.get(obj._value, prop)
                     : undefined,
-                nextPath = Array.isArray(receiver.$value)
-                	? `${receiver.$path}[${prop}]`
-                    : `${receiver.$path}.${prop}`;
+                nextPath = Array.isArray(obj._value)
+                	? `${obj._path}[${prop}]`
+                    : `${obj._path}.${prop}`;
             return obj._nextValue(nextValue, nextPath);
         }
     };
--- src/valueProxy.test.js
+++ src/valueProxy.test.js
@@ -18,60 +18,63 @@ describe('ValueProxy', () => {
     });
 
     test('privates are not accessible', () => {
+        expect(root._value).toBeUndefined();
+        expect(root._path).toBeUndefined();
+        expect(root._error).toBeUndefined();
         expect(root._nextValue).toBeUndefined();
         expect(root._lodashDecorator).toBeUndefined();
     });
 
     describe('valid data', () => {
         test('starts with the full value', () => {
-            expect(root.$value).toBe(mockData);
-            expect(root.$error).toBeUndefined();
+            expect(root.$value()).toBe(mockData);
+            expect(root.$error()).toBeUndefined();
         });
         test('single property access', () => {
             const result = root.metadata;
-            expect(result.$value).toBe(mockData.metadata);
-            expect(result.$error).toBeUndefined();
+            expect(result.$value()).toBe(mockData.metadata);
+            expect(result.$error()).toBeUndefined();
         });
         test('multiple property access', () => {
             const result = root.metadata.url;
-            expect(result.$value).toBe(mockData.metadata.url);
-            expect(result.$error).toBeUndefined();
+            expect(result.$value()).toBe(mockData.metadata.url);
+            expect(result.$error()).toBeUndefined();
         });
         test('array, object and primitive props', () => {
             const result = root.channels[0].programs[0].isLive;
-            expect(result.$value).toBe(mockData.channels[0].programs[0].isLive);
-            expect(result.$error).toBeUndefined();
+            expect(result.$value()).toBe(mockData.channels[0].programs[0].isLive);
+            expect(result.$error()).toBeUndefined();
         });
     });
 
     describe('missing data', () => {
         test('single missing property', () => {
             const result = root.foo;
-            expect(result.$value).toBeUndefined();
-            expect(result.$error.$path).toBe('root.foo');
-            expect(result.$error.$lastPath).toBe('root');
-            expect(result.$error.$lastValue).toBe(mockData);
+            expect(result.$value()).toBeUndefined();
+            expect(result.$error().$path).toBe('root.foo');
+            expect(result.$error().$lastPath).toBe('root');
+            expect(result.$error().$lastValue).toBe(mockData);
         });
         test('chain of missing properties', () => {
             const result = root.foo.bar;
-            expect(result.$value).toBeUndefined();
-            expect(result.$error.$path).toBe('root.foo');
-            expect(result.$error.$lastPath).toBe('root');
-            expect(result.$error.$lastValue).toBe(mockData);
+            expect(result.$value()).toBeUndefined();
+            expect(result.$error().$path).toBe('root.foo');
+            expect(result.$error().$lastPath).toBe('root');
+            expect(result.$error().$lastValue).toBe(mockData);
         });
         test('missing property later in the chain', () => {
             const result = root.channels[0].foo.bar;
-            expect(result.$value).toBeUndefined();
-            expect(result.$error.$path).toBe('root.channels[0].foo');
-            expect(result.$error.$lastPath).toBe('root.channels[0]');
-            expect(result.$error.$lastValue).toBe(mockData.channels[0]);
+            expect(result.$value()).toBeUndefined();
+            expect(result.$error().$path).toBe('root.channels[0].foo');
+            expect(result.$error().$lastPath).toBe('root.channels[0]');
+            expect(result.$error().$lastValue).toBe(mockData.channels[0]);
         });
         test('missing array index', () => {
             const result = root.channels[999];
-            expect(result.$value).toBe(undefined);
-            expect(result.$error.$path).toBe('root.channels[999]');
-            expect(result.$error.$lastPath).toBe('root.channels');
-            expect(result.$error.$lastValue).toBe(mockData.channels);
+            expect(result.$value()).toBe(undefined);
+            expect(result.$error().$path).toBe('root.channels[999]');
+            expect(result.$error().$lastPath).toBe('root.channels');
+            expect(result.$error().$lastValue).toBe(mockData.channels);
         });
     });
 
@@ -93,18 +96,18 @@ describe('ValueProxy', () => {
     describe('$_find', () => {
         test('finds object by id', () => {
             const result = root.channels.$_find({ id: 'c1' }).name;
-            expect(result.$value).toBe(mockData.channels[0].name);
-            expect(result.$error).toBeUndefined();
+            expect(result.$value()).toBe(mockData.channels[0].name);
+            expect(result.$error()).toBeUndefined();
         });
         test('gives undefined for missing id', () => {
             const result = root.channels.$_find({ id: 'foo' });
-            expect(result.$value).toBeUndefined();
-            expect(result.$error.$path).toBe('root.channels.$_find({"id":"foo"})');
+            expect(result.$value()).toBeUndefined();
+            expect(result.$error().$path).toBe('root.channels.$_find({"id":"foo"})');
         });
         test('gives undefined when called on undefined', () => {
             const result = root.foo.$_find({ id: 'c1' });
-            expect(result.$value).toBeUndefined();
-            expect(result.$error.$path).toBe('root.foo');
+            expect(result.$value()).toBeUndefined();
+            expect(result.$error().$path).toBe('root.foo');
         });
     });
 
